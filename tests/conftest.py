e575ba211ac6fb29f1d3a37d5673e3b7213f0c64
LGS-NET\schrma <marco.schroeder@leica-geosystems.com>
2022-09-13 10:42:43 +0200

    Correct flake8

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
-import os
 import inspect
+import os
 from collections import namedtuple
 
 import pytest
@@ -10,8 +10,9 @@ CURRENT_DIR = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfra
 
 @pytest.fixture(name="test_folder")
 def test_folder_fixture():
-    folders = TestFolders(os.path.join(CURRENT_DIR, "input_data"),
-                          os.path.join(CURRENT_DIR, "output_data"))
+    folders = TestFolders(
+        os.path.join(CURRENT_DIR, "input_data"), os.path.join(CURRENT_DIR, "output_data")
+    )
     for folder in filter(lambda f: not os.path.exists(f), folders):
         os.makedirs(folder)
     return folders
